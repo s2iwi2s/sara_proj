29b3501c5fcc0e97755a43f7ec14734e804c9316
s2iwi2s <s2iwi2s@gmail.com>
2020-10-25 16:49:32 -0500

    add react build

---
+++ sara-web/react/build/precache-manifest.84cd92860f4d2f481538bee63c54f889.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95fe7141d568e1d2e1c5ca9bd5347f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c11527e4ba07cb22ea6",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f38b3266f128fa200420",
+    "url": "/static/css/main.25696c6e.chunk.css"
+  },
+  {
+    "revision": "7c11527e4ba07cb22ea6",
+    "url": "/static/js/2.1a3b6d26.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.1a3b6d26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38b3266f128fa200420",
+    "url": "/static/js/main.afbbd309.chunk.js"
+  },
+  {
+    "revision": "041621c0757848d23643",
+    "url": "/static/js/runtime-main.89ce72de.js"
+  }
+]);
