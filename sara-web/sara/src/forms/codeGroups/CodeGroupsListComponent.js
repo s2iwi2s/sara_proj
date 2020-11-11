e3093e74fcf1472c391b0b57082c975dfa5d576d
s2iwi2s <s2iwi2s@gmail.com>
2020-11-10 19:22:55 -0600

    fix school level query

--- sara-web/sara/src/forms/codeGroups/CodeGroupsListComponent.js
+++ sara-web/sara/src/forms/codeGroups/CodeGroupsListComponent.js
@@ -98,6 +98,10 @@ export default function CodeGroupsListComponent(props) {
     {
       field: 'description',
       headerName: 'Description',
+    },
+    {
+      field: 'priority',
+      headerName: 'Priority',
     }
   ];
 
