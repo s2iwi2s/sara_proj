f4a1727361ca963dd857ddfa6143e81a500c047a
s2iwi2s <s2iwi2s@gmail.com>
2020-11-27 22:50:10 -0600

    add acct payables settings

--- sara-service/src/main/java/com/sara/service/impl/AccountPayablesSettingsServiceImpl.java
+++ sara-service/src/main/java/com/sara/service/impl/AccountPayablesSettingsServiceImpl.java
@@ -1,10 +1,15 @@
 package com.sara.service.impl;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.sara.data.document.AccountPayablesSettings;
 import com.sara.data.document.QAccountPayablesSettings;
@@ -18,7 +23,8 @@ import com.sara.service.SequenceGeneratorService;
 public class AccountPayablesSettingsServiceImpl extends AbstractService<AccountPayablesSettings, String> {
 	Logger log = LoggerFactory.getLogger(this.getClass());
 
-	public AccountPayablesSettingsServiceImpl(AccountPayablesSettingsMongoRepository repo, SequenceGeneratorService sequenceGeneratorService) {
+	public AccountPayablesSettingsServiceImpl(AccountPayablesSettingsMongoRepository repo,
+			SequenceGeneratorService sequenceGeneratorService) {
 		super(repo, sequenceGeneratorService);
 	}
 
@@ -38,9 +44,24 @@ public class AccountPayablesSettingsServiceImpl extends AbstractService<AccountP
 	public AccountPayablesSettings getNewEntity() {
 		return new AccountPayablesSettings();
 	}
+
 	@Override
 	public AccountPayablesSettings save(AccountPayablesSettings entity, School school) {
 		entity.setSchool(school);
 		return super.save(entity, school);
+	}
+
+	public List<AccountPayablesSettings> findByApplyToAllList(School school) {
+		return ((AccountPayablesSettingsMongoRepository) repo).findByActiveAndApplyToAllAndSchool(true, true, school);
+		// return findAll(pageable);
+	}
+	
+	public Page<AccountPayablesSettings> findAllActiveList(String searchValue, Pageable pageable, User user) {	
+		BooleanBuilder searchbb = new BooleanBuilder();
+		searchbb.and(QAccountPayablesSettings.accountPayablesSettings.applyToAll.eq(false));
+		searchbb.and(QAccountPayablesSettings.accountPayablesSettings.active.eq(true));
+		searchbb.and(QAccountPayablesSettings.accountPayablesSettings.description.containsIgnoreCase(searchValue));
+		searchbb.and(QAccountPayablesSettings.accountPayablesSettings.school.eq(user.getSchool()));
+		return repo.findAll(searchbb.getValue(), pageable);
 	}
 }
