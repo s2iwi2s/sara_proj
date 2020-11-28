889421f0bb947f8b44ed5726cb8298e6c3464009
s2iwi2s <s2iwi2s@gmail.com>
2020-11-27 22:49:40 -0600

    add acct payables settings

--- sara-data/src/main/java/com/sara/data/repository/AccountPayablesSettingsMongoRepository.java
+++ sara-data/src/main/java/com/sara/data/repository/AccountPayablesSettingsMongoRepository.java
@@ -1,6 +1,11 @@
 package com.sara.data.repository;
 
+import java.util.List;
+
 import com.sara.data.document.AccountPayablesSettings;
+import com.sara.data.document.School;
 
 public interface AccountPayablesSettingsMongoRepository extends CustomRepository<AccountPayablesSettings, String> {
+	List<AccountPayablesSettings> findByActiveAndApplyToAllAndSchool(boolean active, boolean applyToAll, School school);
+	
 }
